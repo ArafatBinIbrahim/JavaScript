749728b9356fd36be8233d87f26b18bd46b0d991
ArafatIbrahim <168175815+ArafatBinIbrahim@users.noreply.github.com>
2024-10-09 04:30:48 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,198 @@
+// text change 1
+function textChange() {
+
+    document.getElementById('tc').innerHTML = "MY NIGGA";
+
+}
+// show time 2
+function showTime() {
+
+    document.getElementById('st').innerHTML = Date();
+
+}
+// color change 3
+function colorChange() {
+
+    document.getElementById('clr').style.color = "red";
+
+}
+// font size change 4
+function fontSize() {
+
+    document.getElementById('f_size').style.fontSize = "50px";
+
+}
+// font family 5
+function fontFamily() {
+
+    document.getElementById('ff').style.fontFamily = "cursive";
+
+}
+
+// display show hide 6
+function displayShow() {
+
+    document.getElementById('boxshow').style.display = "block";
+
+}
+
+function displayHide() {
+
+    document.getElementById('boxshow').style.display = "none";
+
+}
+// border radius 7
+function raidus() {
+
+    document.getElementById('raidus').style.borderRadius = "30px";
+
+}
+// circle 8
+function circle() {
+
+    document.getElementById('circle').style.borderRadius = "50%";
+
+}
+// border 9
+function border() {
+
+    document.getElementById('border').style.border = "5px solid black";
+
+}
+// radius top-left bottom-right 10 
+function radius_tl_br() {
+
+    document.getElementById('tl_br').style.borderRadius = "25% 10%";
+
+}
+// radius top-right bottom-left 11
+function radius_tb_bl() {
+
+    document.getElementById('tr_bl').style.borderRadius = "10% 25%";
+
+}
+// square 12
+function squreBox() {
+
+    document.getElementById('sqr_box').style.borderRadius = "0%";
+
+}
+// undrline 13
+function underLine() {
+
+    document.getElementById('undr').style.textDecoration = "underline";
+
+}
+// mark 14 
+function mark() {
+
+    document.getElementById('mark').style.color = "orangered";
+
+}
+// upper lower case 15
+function uppcase() {
+
+    document.getElementById('upp_lw').style.textTransform = "uppercase";
+
+}
+
+function lowcase() {
+
+    document.getElementById('upp_lw').style.textTransform = "lowercase";
+
+}
+
+
+// alart 16
+function alartBox() {
+
+    alert('Meow Meow NIGGA:-) ');
+
+}
+// blub in/off 17
+function blubOn() {
+
+    document.getElementById('blub').src = "images/on.gif";
+
+}
+
+function blubOff() {
+
+    document.getElementById('blub').src = "images/off.gif";
+
+}
+// cold / hot coffe 18
+function coldCoffee() {
+
+    document.getElementById('coffee').src = "images/cold.png";
+
+}
+
+function hotCoffee() {
+
+    document.getElementById('coffee').src = "images/hot.png";
+
+}
+// happy sad 19
+function hp_sd() {
+
+    document.getElementById('happy').src = "images/happy.png";
+
+}
+
+function sd_hp() {
+
+    document.getElementById('happy').src = "images/sad.png";
+
+}
+// cut inside line  20
+function line_throug() {
+
+    document.getElementById('line_throug').style.textDecoration = "line-through ";
+
+}
+// over line 21
+function overline() {
+    document.getElementById('overline').style.textDecoration = "overline";
+}
+// letter spacing 22  
+function lettterSpacing() {
+    document.getElementById('lettterSpacing').style.letterSpacing = "10px";
+}
+// word specing 23
+function wordSpecing() {
+    document.getElementById('wordSpacing').style.wordSpacing = "20px";
+}
+// text align start 24
+function textAlingStart() {
+    document.getElementById('textAlignStrat').style.textAlign = "left";
+}
+// text align end  25
+function textAlignEnd() {
+    document.getElementById('textAlignEnd').style.textAlign = "right";
+}
+// text aling center 26
+function textAlignCenter() {
+    document.getElementById('textAlignCenter').style.textAlign = "center";
+}
+// font weight 27
+function fontWeight() {
+    document.getElementById('font_weight').style.fontWeight = "700";
+}
+
+// border doted 28
+function bdrTop() {
+    document.getElementById('bdrTop').style.borderTop = "3px dashed red";
+}
+// border doted
+function dotid() {
+    document.getElementById('brdrDoted').style.borderLeft = "3px dashed red";
+}
+// border solid
+function solid() {
+    document.getElementById('solid').style.borderRight = "3px solid red";
+}
+// border bottom
+function bottom() {
+    document.getElementById('bottom').style.borderBottom = "3px solid red";
+}
